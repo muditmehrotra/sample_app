f9c39d23788bcf3f12bacf2f42eb56d3aac51863
Mudit Mehrotra <mudit.mehrotra@triffort.com>
2015-04-15 19:08:27 +0530

    Finish static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+#return the full title on the per page basis
+def full_title(page_title = '')
+	base_title = 'Ruby on Rails tutorials sample app'
+	if page_title.empty?
+		base_title
+	else
+		"#{page_title} | #{base_title}"
+	end
+end
+
 end
